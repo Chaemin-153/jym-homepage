3c2314c13bd71b1b6f004815998b8708dfd28334
Chaemin-153 <0520032@gmail.com>
2024-09-07 00:58:20 +0900

    fix: "나의 젊음을 주님께" 뒤에 느낌표 더 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,8 @@ export default function Home() {
     <div>
       <div className=" text-6xl text-red-600">Welcome to</div>
       <div className=" text-4xl text-blue-600">JOYFUL YOUTH MISSION</div>
-      <div className=" text-2xl text-pink-600">나의 젊음을 주님께!</div>
+      <div className=" text-2xl text-pink-600">나의 젊음을 주님께!!</div>
       <Image src={jesus} alt="Jesus is Huging" width={300} height={300} />
     </div>
   );
 }
-
